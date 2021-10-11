1cd54bf94f464ca7ade4560879723414b9399a8c
Nguyen Thanh Tuan <tuan.nguyen@morphotech.co.uk>
2021-10-11 16:23:30 +0700

    add unit test for services/trader

--- src/services/traders.test.js
+++ src/services/traders.test.js
@@ -1,7 +1,12 @@
-import { getTraders } from "./traders";
+import {
+  getTrader,
+  getTraderContact,
+  getTraderKYCResult,
+  getTraders,
+} from "./traders";
 import { KYCStatus } from "models/kyc";
 test("getTraders should work", () => {
-  expect(getTraders()).resolves.toReturnWith([
+  expect(getTraders()).resolves.toStrictEqual([
     {
       id: "1",
       marketplace: "Hotel Supply B2B",
@@ -57,6 +62,41 @@ test("getTraders should work", () => {
       email: "tuan@tuan.tuan  ",
       dateJoined: "2021-10-11",
       status: KYCStatus.PASSED,
+    },
+  ]);
+});
+
+test("getTrader should work", () => {
+  expect(getTrader("1")).resolves.toStrictEqual({
+    id: "1",
+    marketplace: "Hotel Supply B2B",
+    username: "tuan",
+    email: "tuan@tuan.tuan  ",
+    dateJoined: "2021-10-11",
+    status: KYCStatus.PASSED,
+  });
+});
+
+test("getTraderContact should work", () => {
+  expect(getTraderContact()).resolves.toStrictEqual({
+    companyName: "AAA",
+    phoneNumber: "12312451",
+    email: "email@asd.cos",
+    country: "Vietnam",
+    address: "232 Akd ale",
+    emailVerified: true,
+  });
+});
+
+test("getTraderKYCResult should work", () => {
+  expect(getTraderKYCResult()).resolves.toStrictEqual([
+    {
+      id: "1",
+      date: "2021-1-1-2",
+      type: "string",
+      provider: "Raputel",
+      status: KYCStatus.PASSED,
+      message: "My message",
     },
   ]);
 });
