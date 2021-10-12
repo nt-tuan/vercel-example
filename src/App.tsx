32f4d783746ee66e4781ba4cdedec3e3aecbf171
thanhtuan <tuan.nguyen@morphotech.co.uk>
2021-10-12 15:08:33 +0700

    update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { AdminLayout } from "layouts/AdminLayout";
 import { AppRouter } from "./AppRouter";
-import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
