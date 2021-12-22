3067658cefc7da1a03b1602e816f3cd0a485a737
Nguyen Thanh Tuan <tuan@Nguyens-MBP.finbase.net>
2021-12-22 11:09:48 +0700

    fix: responsive

--- src/layouts/AdminLayout.test.tsx
+++ src/layouts/AdminLayout.test.tsx
@@ -1,9 +1,14 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { AdminLayout } from "./AdminLayout";
+
 jest.mock("./SideMenu", () => ({
   SideMenu: () => <div>side-menu</div>,
 }));
+
 test("AdminLayout should work", () => {
-  const { getByText } = render(<AdminLayout>children</AdminLayout>);
+  const { getByText, getByTestId } = render(
+    <AdminLayout>children</AdminLayout>
+  );
+  fireEvent.click(getByTestId("ChevronLeftIcon"));
   expect(getByText("children")).toBeInTheDocument();
 });
